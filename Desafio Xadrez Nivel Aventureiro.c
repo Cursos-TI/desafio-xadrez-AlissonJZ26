909d09a95e318f5bac7946a10338b1917453633b
AlissonJZ26 <Alissonzeni160@gmail.com>
2025-03-14 00:27:10 +0000

    criei o nivel Aventureiro

---
+++ Desafio Xadrez Nivel Aventureiro.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+
+    int i, j;
+     while (i <=1)
+     {
+       for(int i =1; i < 1; i++){
+        printf("Cima\n");
+        i++;
+       }
+       printf("Baixo\n");
+       i++;
+     } 
+      printf ("Esquerda\n");
+      j++;
+     {
+       printf("Direita\n");
+       j++;
+       }
+     return 0;
+     
+ }
